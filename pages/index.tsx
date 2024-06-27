97862dc707ebdb7a382a02c6a52a592f56015c59
Gabriel Amaral <gabrielvieira2205@gmail.com>
2024-06-27 09:45:29 -0300

    Criando seção de apresentação - Parte 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,10 @@ const HomeNotAuth = function () {
                 />
             </Head>
             <main>
-                <HeaderNoAuth />
-                <PresentationSection />
+                <div className={styles.sectionBackground}>
+                    <HeaderNoAuth />
+                    <PresentationSection />
+                </div>
             </main>
         </>
     )
