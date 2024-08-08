f51574a57f596e62831301123b01bc455d200480
Gabriel Amaral <gabrielvieira2205@gmail.com>
2024-08-08 10:30:13 -0300

    Criação da página de Search

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,17 +4,23 @@ import HeaderAuth from "../src/components/common/headerAuth"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import courseService, { CourseType } from "@/services/courseService"
+import { Container } from "reactstrap"
+import SearchCard from "@/components/searchCard"
+import Footer from "@/components/common/footer"
 
 const Search = function () {
     const router = useRouter()
-    const searchName: any = router.query.name
+    const searchName = router.query.name
     const [searchResult, setSearchResult] = useState<CourseType[]>([])
 
     const searchCourses = async function () {
-        if (searchName === "string") {
-            const res = await courseService.getSearch(searchName)
-
-            setSearchResult(res.data.courses)
+        if (searchName && typeof searchName === "string" && searchName.trim().length > 0) {
+            try {
+                const res = await courseService.getSearch(searchName)
+                setSearchResult(res.data.courses)
+            } catch (error) {
+                console.error("Erro ao buscar cursos:", error)
+            }
         }
     }
 
@@ -28,13 +34,24 @@ const Search = function () {
                 <title>Onebitflix - {searchName}</title>
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
             </Head>
-            <main>
-                <HeaderAuth />
-                {searchResult?.map((course) => (
-                    <div key={course.id}>
-                        <p>{course.name}</p>
+            <main className={styles.main}>
+                <div className={styles.headerFooterBg}>
+                    <HeaderAuth />
+                </div>
+                {searchResult.length >= 1 ? (
+                    <div className={styles.searchResult}>
+                        <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+                            {searchResult?.map((course) => (
+                                <SearchCard key={course.id} course={course} />
+                            ))}
+                        </Container>
                     </div>
-                ))}
+                ) : (
+                    <p className={styles.noSearchText}>Nenhum resultado encontrado!</p>
+                )}
+                <div className={styles.headerFooterBg}>
+                    <Footer />
+                </div>
             </main>
         </>
     )
